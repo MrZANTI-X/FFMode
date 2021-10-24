d30c6871d4e35d7ece63c34d1246d587d6879d9a
MRZANTI-X <81058479+MrZANTI-X@users.noreply.github.com>
2021-10-25 04:32:55 +0530

    Add files via upload

---
+++ Freefier.rb
@@ -0,0 +1,51 @@
+#free fier mod manu apk / aimbit  telkill , tallport 
+  $r = "\033[1;91m"
+  $g = "\033[1;92m"
+  $y = "\033[1;93m"
+  $p = "\033[1;94m"
+  $m = "\033[1;95m"
+  $c = "\033[1;96m"
+  $w = "\033[1;97m"
+  $a = "\033[1;0m"
+module Most
+puts "  }•••• ( ZANTI ) ••••{  "
+sleep (0.1)
+puts ""
+sleep (0.1)
+puts "     }••MODE-MANU••{     "
+puts ""
+sleep (0.1)
+puts ""
+puts "#{$r}( #{$y}1 #{$r}) #{$g}Free-fire Mod+apk Link  "
+sleep (0.1)
+puts "#{$r}( #{$y}2 #{$r}) #{$g}Going Telegram "
+sleep (0.1)
+puts "#{$r}( #{$y}3 #{$r}) #{$p}Subscribe "
+sleep (0.1)
+puts"#{$r}( #{$y}4 #{$r}) #{$g}INSTALLATION & OBB TOOL GP"
+sleep (1)
+puts "#{$r}[#{$y} 0 #{$r}] #{$g}UPDATE TOOL "
+print "#{$r}[ #{$y}? #{$r}] #{$p}Enter any one : #{$w}" ; $ans = gets.chomp
+		case $ans
+	when '1' 
+	puts "Free Fier Mod Manu" 
+        puts "#{$r}Mod Manu version {  #{$y}V25   #{$r}}#{$w}"
+	sleep (0.1) 
+	puts "#{$m}TAP TO OPEN LINK #{$w} "
+	a = gets
+	system"xdg-open --view https://bit.ly/3ndxxYg" 
+	when '2'
+	system"xdg-open --view https://t.me/akt_gaming_mod"
+	when '3'
+	system"xdg-open --view https://youtube.com/channel/UCFm8ZvT8ZZ6RpqpWKRy9ZMg"
+	when '4'
+	puts "#{$m} TAP-TO-OPEN LINK "
+	s = gets
+	system"xdg-open --view https://bit.ly/2Xnx6Sg"
+	when '0'
+	system"cd" ; system"rm -rf FFMode" ; system"git clone https://github.com/MrZANTI-X/FFMode.git"
+	else
+	puts "SYSTEM error ? "
+		end
+end
+
